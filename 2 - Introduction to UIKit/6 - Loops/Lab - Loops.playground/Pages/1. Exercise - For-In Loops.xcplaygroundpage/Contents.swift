96f6b55ab486786d9fc60c3aa63c62ceb430b2dc
Ziterz <ziady.mubaraq@gmail.com>
2024-01-16 00:58:33 +0700

    complete the Lab - Loops

--- 2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -3,14 +3,22 @@
  
  Create a for-in loop that loops through values 1 to 100, and prints each of the values.
  */
-
+for i in 1...100 {
+  print(i)
+}
 
 //:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-
+for i in alphabet {
+  print(i)
+}
 
 //:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
+let dict: [String: String] = ["DKI Jakarta": "Jakarta", "Jawa Barat": "Bandung", "Kalimantan Barat": "Pontianak"]
 
+for (key, value) in dict {
+  print("\(value), \(key)")
+}
 
 /*:
 page 1 of 6  |  [Next: App Exercise - Movements](@next)
--- 2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
@@ -5,10 +5,20 @@
  */
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
+for i in alphabet {
+  print(i)
+  if i == "R" {
+    break
+  }
+}
 //:  Create a `[String: String]` dictionary where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, with one of them being your home state. Now loop through this dictionary again, printing out the keys and values in a sentence, but add an if statement that will check if the current iteration is your home state. If it is, print("I found my home!") and break out of the loop.
-
-
+var dict: [String: String] = ["DKI Jakarta": "Jakarta", "Jawa Barat": "Bandung", "Kalimantan Barat": "Pontianak"]
+for word in dict {
+  if word.key == "DKI Jakarta" {
+    print("I found my home!")
+    break
+  }
+}
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Finding Movements](@next)
  */
